377a7033280f1477b9781df3acd67c5e68162c3b
Nilton <niltoneapontes@gmail.com>
2022-12-16 00:34:19 -0300

    feat: improve warning colors

---
+++ index.ts
@@ -0,0 +1,10 @@
+const myGrades = [5, 10, 9];
+
+const calculateAverage = (grades: number[]) => {
+  const sum = grades.reduce((acc, value) => acc += value, 0);
+  const exams = grades.length;
+
+  return sum/exams;
+}
+
+calculateAverage(myGrades);
