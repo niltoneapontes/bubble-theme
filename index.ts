ff8c582caa962c3a86ee05bbc42a73af3cde5e8f
Nilton <niltoneapontes@gmail.com>
2022-12-16 10:19:18 -0300

    fix: purple tone

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 const myGrades = [5, 10, 9];
 
-const calculateAverage = (grades: number[]) => {
-  const sum = grades.reduce((acc, value) => acc += value, 0);
-  const exams = grades.length;
 
-  return sum/exams;
+export class MyFunctions {
+  constructor() {
+  }
+  calculateAverage = (grades: number[]) => {
+    const sum = grades.reduce((acc, value) => acc += value, 0);
+    const exams = grades.length;
+  
+    return sum/exams;
+  }
 }
 
-calculateAverage(myGrades);
+const myFunctions = new MyFunctions();
+
+myFunctions.calculateAverage(myGrades);
