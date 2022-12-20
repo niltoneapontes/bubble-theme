9cfb35d233b548a41e18eddcf8824faacf0297a1
Nilton <niltoneapontes@gmail.com>
2022-12-20 17:38:29 -0300

    feat: create soft theme

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ const myGrades = [5, 10, 9];
 export class MyFunctions {
   constructor() {
   }
+  
   calculateAverage = (grades: number[]) => {
     const sum = grades.reduce((acc, value) => acc += value, 0);
     const exams = grades.length;
